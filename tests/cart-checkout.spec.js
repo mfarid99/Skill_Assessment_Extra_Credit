36e8113f28515c78238eb074bcfcbfca68184362
Moe <moe@triblio.com>
2025-06-17 12:32:03 -0400

    lipstick added to cart

--- tests/cart-checkout.spec.js
+++ tests/cart-checkout.spec.js
@@ -18,14 +18,12 @@ test('Should create new user account', async ({ page }) => {
     await page.click('button[title="Login"]');
     await expect(page.locator('div.menu_text')).toContainText('Mohamed');
     //Click on Apparel
-    // await page.click('a:has-text("Apparel")');
     await page.getByRole('link', {name: /apparel/i}).click();
     //Click on T-shirts
     await page.click('a:has-text("T-shirts")');
     //Click on Formal Double Cuffs shirt
     const productLinkTshirt = page.locator('a[title="Designer Men Casual Formal Double Cuffs Grandad Band Collar Shirt Elegant Tie"]');
     await productLinkTshirt.click()
-    // await page.locator('a:has-text("Formal Double Cuffs")').click();
     await expect(page.locator('h1')).toContainText("Formal Double Cuffs");
     //Update Shirt quantity to 2
     await page.fill('input[name="quantity"]', "2");
@@ -45,7 +43,28 @@ test('Should create new user account', async ({ page }) => {
         tax: '$5.44',
         total: '$71.44'
     })
-
+    //Click makeup tab
+    await page.getByRole('link', {name: /makeup/i}).click();
+    //Click Lips
+    await page.click('a:has-text("Lips")');
+    //Click Viva Glam Lipstick
+    await page.click('[title="Viva Glam Lipstick"]');
+     //Add to cart
+    await page.click('a:has-text("Add to Cart")');
+    //Assert item added to shopping cart
+    await assertItemInCart(page, {
+        name: "Viva Glam Lipstick",
+        unitPrice: '5.00',
+        quantity: '1',
+        total: '$5.00'
+    })
+    //Assert item total is accurate
+    await assertCartTotal(page, {
+        subtotal: '$69.00',
+        shipping: '$2.00',
+        tax: '$5.87',
+        total: '$76.87'
+    })
 
 
 
